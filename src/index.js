51bcba2389bc5c268fbcaea563253bec1e7c222a
Loktysh Alexandr <vebster24@mail.ru>
2021-02-01 12:02:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stackArr = new Array;
+  let bracketsObj = Object.fromEntries(bracketsConfig);
+  str = str.split('');
+  let i = 0;
+  while (i <= str.length) {
+    let el = bracketsObj[str[i]];
+      if (stackArr[0] === str[i]) {
+          stackArr.shift();
+      } else if (el) {
+          stackArr.unshift(el);
+      } else {
+          return false;
+      }
+    i++;
+  }
+  return stackArr.length === 0;
 }
